2702dc993ceb2eeae46659c6a05e272a38ead0c3
vagonbar <vagonbar@gmail.com>
2014-07-19 20:39:52 -0300

    utils/addlicense.py: script to add license notice to modules

--- gwn/epydoc_doc/epydoc_example.py
+++ gwn/epydoc_doc/epydoc_example.py
@@ -1,6 +1,24 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 #
+#    This file is part of GNUWiNetwork,
+#    Copyright (C) 2014 by 
+#        Pablo Belzarena, Gabriel Gomez Sena, Victor Gonzalez Barbone,
+#        Facultad de Ingenieria, Universidad de la Republica, Uruguay.
+#
+#    GNUWiNetwork is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    GNUWiNetwork is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with GNUWiNetwork.  If not, see <http://www.gnu.org/licenses/>.
+#
 # epydoc_example.py : example code for the use of Epydoc
 #
 
@@ -39,7 +57,6 @@ class ClassA:
         '''A brief description of the constructor, if necessary.
     
         A detailed description of the constructor, if necessary.
-
         '''
         self.in_attr1 =  None   # an instance attribute, 'self' before
 
--- gwn/mkdoc.sh
+++ gwn/mkdoc.sh
@@ -1,19 +1,10 @@
 #!/bin/bash
 # mkdoc.sh: makes epydoc
 
-#ALLSUBDIRS="bitacora_belza.txt|doc|draft|grc|gwn-companion.sh|html|__init__.py|__init__.pyc|libadaptationlayer|libadaptlay80211|libevents|libframes|libfsm|libgnuradio|libgwnblocks|libgwnBlocks|libMAC|libmac80211|libmacTDMA|libmanagement|libtimer|libutils|libvirtualchannel|logs|mkdoc.sh|runtests.py|runtests.pyc|runtests.sh|scripts|xml|"
 
-EXCLUDES="viejos|otros|old|others|draft|libgwnBlocks|logs"
+EXCLUDES="viejos|otros|old|others|draft|logs"
 
-#EXCLUDES=${EXCLUDES}"|bitacora_belza.txt"
-#EXCLUDES=${EXCLUDES}"|mac_action"
-EXCLUDES=${EXCLUDES}"|libgnuradio|grc|scripts|xml"
-#EXCLUDES=${EXCLUDES}"|libfsm|libmanagement|libtimer"
-
-#EXCLUDES="bitacora_belza.txt|draft|grc|gwn-companion.sh|libadaptationlayer|libadaptlay80211|libevents|libframes|libfsm|libgnuradio|libgwnBlocks|libMAC|libmac80211|libmacTDMA|libmanagement|libtimer|libvirtualchannel|logs|runtests.py|scripts|xml"
-
-
-
+EXCLUDES=${EXCLUDES}"|gwnc|scripts|xml"
 
 if [ ! "$1" ]
 then
@@ -29,20 +20,4 @@ else
   mkdir html
 fi
 
-### ERROR: with full content nothing was generated!
-# opted for generating only updated versions of code
-#echo "  excluded:" $EXCLUDES
-#epydoc -v --name $PRJNM -o ./html --exclude "$EXCLUDES" gn
-
-
-# the files to include in doc generation
-
-#INCLUDES="__init__.py libgwnblocks doc libvirtualchannel"
-#epydoc -v --name $PRJNM $INCLUDES
-#/usr/share/pyshared/epydoc/cli -v --name $PRJNM
-
-PYTHONPATH=$PYTHONPATH:/usr/share/pyshared/
-env PYTHONPATH=$PYTHONPATH:/usr/share/pyshared/ python /usr/share/pyshared/epydoc/cli.py -v --name GNUWiNetwork --exclude "gwnc" .
-
-
-
+epydoc -v --name $PRJNM -o ./html --exclude "$EXCLUDES" .
